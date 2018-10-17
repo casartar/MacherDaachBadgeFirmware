f2707cf9246c8698c5ef8c5e42829b66c9b2f398
holymoly <holzmolz@googlemail.com>
2018-10-17 15:49:53 +0200

    added simple pong

--- MacherDaachBadgeFirmware.ino
+++ MacherDaachBadgeFirmware.ino
@@ -154,7 +154,8 @@ void loop() {
   // Call your output mode in this switch
   switch (outputMode){
     case FILL_MATRIX_SLOW:
-      outputShiftString(TEXT);
+      pong();
+      //outputShiftString(TEXT);
       //outputString(TEXT);
       //displayCharacterOffset(LETTERS[0],3,0);
       //output_fill_matrix_slow();
@@ -351,23 +352,62 @@ void outputShiftString(char * text){
   }
 }
 
-void displayCharacter(const byte* image) {
-  for (int i = 0; i < 8; i++) {
-    matrix[8-i] = image[i];
+void pong(){
+  static uint8_t speed = 100;
+  static bool start_left = true;
+  //static uint8_t difficult_a = 0;
+  //static uint8_t difficult_b = 0;
+  static uint8_t points_a = 0;
+  static uint8_t points_b = 0;
+  static uint8_t pos_act = 0;
+  static bool direction = 0; // 0 = right
+  static uint8_t fields_valid = 2; // rebounce if button was pressed 
+
+  // check rebounce
+  if(direction == false && (pos_act > 7-fields_valid) && button_2_state == BUTTON_HELD || direction == true && (pos_act < 0 + fields_valid)  && button_1_state == BUTTON_HELD){
+    direction = !direction;
+  }
+  
+  if (countdown == 0){
+
+    if(direction){
+      pos_act--;
+    }else{
+      pos_act++;
+    }
+  
+    // check for points
+    // act_pos is unsigned -> values greater 7 is a point for A or B depending on direction
+    if(pos_act > 7){
+      if (direction){
+        points_b++;
+      }else{
+        points_a++;
+      }
+      
+      start_left != start_left;
+      if(start_left){
+        pos_act=0;
+        direction=0;
+      }else{
+        pos_act=7;
+        direction=1;
+      }
+      speed =100;
+    }
+
+    if(points_a > 7 || points_b > 7){
+      points_a=0;
+      points_b=0;
+    }
+    // reset last frame
+    clear_matrix_immediatly_without_reset();
+    // draw new frame
+    matrixSetPixel(pos_act, 4, true);
+    matrixSetPixel(points_a, 0, true);
+    matrixSetPixel(points_b, 7, true);
+    speed--;
+    countdown = speed;
   }
 }
 
-void displayCharacterOffset(const byte* image, int8_t x, int8_t y) {
-  for (int i = 0; i < 8; i++) {
-    for (int j = 0; j < 8; j++) {
-      if(((j+x) >= 0 && (j+x) < 8) && ((i+y) >= 0 && (i+y) < 8)){
-        // set pixel
-        //matrix[8-(i+y)] = pgm_read_byte_near(image + i);
-        
-        //<< j;
-        matrixSetPixel(j+x,7-(i+y), bitRead(pgm_read_byte_near(image + i),7-j));
-        //matrix[i+y][j+x] = bitRead(image[i],7-j);
-      }
-    }
-  }
-}
