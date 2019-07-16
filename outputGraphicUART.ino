69263ddcca4fba51d77cf8f0419ff564390409f0
halbrot hefetopf <git@halbrot.de>
2019-07-16 20:14:45 +0200

    changeg strcpy to strncpy because of buffer overflow (8Bytes + \0 doesn't fit into matrix)

--- outputGraphicUART.ino
+++ outputGraphicUART.ino
@@ -1,7 +1,7 @@
 // helper function for mode-array
 void outputGraphicsUART(){
   if (uartReceiveCompleteFlag) {
-    strcpy((char*)matrix, (char*)uartInputBuffer);
+    strncpy((char*)matrix, (char*)uartInputBuffer,8);
     uartReceiveCompleteFlag = false;
   }
 }
