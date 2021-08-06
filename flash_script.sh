50feb2d2735c8a5c1b671518c44629a30e8f99ec
Peter Scheydt <casartar@web.de>
2021-08-06 23:17:54 +0200

    add flash script

---
+++ flash_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo -n 'Name eingeben: ' ; read name
+
+cp include/main.h include/main.h.backup
+sed -i "/#define TEXT / s/^.*$/#define TEXT \"$name\" /" include/main.h
+
+echo "$name" >> list_of_names.txt 
+
+pio run --target fuses || { echo FEHLER beim setzen der Fuses ; exit 1; }
+pio run --target upload || { echo FEHLER beim compilieren oder flashen ; exit 1; }
+
+mv include/main.h.backup include/main.h
